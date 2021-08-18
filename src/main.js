c83d21cf12d96dd1affc9c8ca8938196b5fb9fac
Lê Thị Yến Thanh <57611055+yenthanhle@users.noreply.github.com>
2021-08-18 14:54:57 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -15,6 +15,9 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'resources/views'))
 app.set('view engine', '.hbs');
 
+
+
+
 app.use(
     express.urlencoded({
         extended: true,
